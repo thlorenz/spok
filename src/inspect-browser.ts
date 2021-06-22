e27e8097e2e59d7dbbe53875bb63613e7a8f8741
Aaron Reisman <lifeiscontent@users.noreply.github.com>
2021-06-22 16:06:43 -0700

    Update inspect-browser.ts
    Fix issue when spok is undefined in browser

--- src/inspect-browser.ts
+++ src/inspect-browser.ts
@@ -2,7 +2,7 @@ import * as util from 'util'
 import spok from './spok'
 
 // terminal colors won't show properly in the browser
-spok.color = false
+if(spok) spok.color = false;
 
 export default function inspect(obj: any, color: boolean) {
   return util.inspect(obj, false, 5, color)
